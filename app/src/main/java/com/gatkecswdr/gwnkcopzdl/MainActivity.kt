fa5f6dc95404ab79f18a6dc58e2fb7473255ffa5
AmitHasan <amit.just.sh@gmail.com>
2021-08-19 00:02:46 +0600

    final

--- app/src/main/java/com/gatkecswdr/gwnkcopzdl/MainActivity.kt
+++ app/src/main/java/com/gatkecswdr/gwnkcopzdl/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.gatkecswdr.gwnkcopzdl
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.get
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import io.flutter.embedding.android.FlutterActivity
-import android.content.Intent
-import android.net.Uri
 
 
 class MainActivity : AppCompatActivity() {
